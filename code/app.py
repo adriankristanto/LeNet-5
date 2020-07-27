1dbb8eaa8616f0ecd6e888f7c11fcb40cd9da0c2
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-27 22:41:37 +1000

    added a python script that accepts custom data instance and makes prediction on it

---
+++ code/app.py
@@ -0,0 +1,43 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import LeNet5 
+import tqdm
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+import numpy as np
+import PIL
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# load the test data
+transform = transforms.Compose(
+    [transforms.Resize((32, 32)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))]
+)
+
+def predict(image_path):
+    # convert the image to a greyscale image
+    image = PIL.Image.open(image_path).convert('L')
+    # resize the image, transform the image to tensor, and normalise it
+    img_tensor = transform(image).unsqueeze_(0)
+    # forward propagation
+    output = net(img_tensor)
+    # return prediction
+    # print(output)
+    return torch.argmax(output, dim=1).item()
+
+if __name__ == "__main__":
+    # load the model
+    FILEPATH = os.path.dirname(os.path.realpath(__file__)) + '/../model/'
+    FILENAME = 'lenet5-0.pth'
+    net = LeNet5.Net()
+    # reference: https://pytorch.org/tutorials/beginner/saving_loading_models.html#saving-loading-model-across-devices
+    net.load_state_dict(torch.load(FILEPATH + FILENAME,  map_location=device))
+
+    # predict image
+    IMAGE_PATH = os.path.dirname(os.path.realpath(__file__)) + '/../sample/'
+    IMAGE_NAME = 'sample0.png'
+    print(predict(IMAGE_PATH + IMAGE_NAME))
